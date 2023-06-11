3fc9dcf0e374e8659367afba7d70e7953afeb053
tsubokura-shuhei <repeat2639@gmail.com>
2023-06-11 11:40:17 +0900

    first commit

---
+++ routers/auth.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const generateIdenticon = require("../utils/generateldenticon");
+
+const prisma = new PrismaClient();
+
+//新規ユーザー登録API
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const defaultIconImage = generateIdenticon(email);
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+      profile: {
+        create: {
+          bio: "初めまして",
+          profileImageUrl: defaultIconImage,
+        },
+      },
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  return res.json({ user });
+});
+
+//ユーザーログインAPI
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ error: "こちらのユーザーは存在しません" });
+  }
+
+  const isPasswordVaild = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordVaild) {
+    return res
+      .status(401)
+      .json({ error: "こちらのパスワードは間違っています" });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return res.json({ token });
+});
+
+module.exports = router;
